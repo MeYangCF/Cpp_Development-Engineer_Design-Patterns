41015269c0fb2c5ad4fdeaf333dfdb1575b0335d
摆烂杨 <2971208523@qq.com>
2024-12-16 10:39:35 +0800

    添加设计模式文件

--- C1_CreationalPatterns/01_Singleton/main.cpp
+++ C1_CreationalPatterns/01_Singleton/main.cpp
@@ -1,6 +1,11 @@
 #include "Singleton.h"
+#include "Singleton1.h"
+#include "Singleton2.h"
+#include "Singleton3.h"
+#include "Singleton4.h"
 
-void main()
+
+int main()
 {
 	Singleton::Instance()->DoSomething();
 	Singleton::DestoryInstance();
